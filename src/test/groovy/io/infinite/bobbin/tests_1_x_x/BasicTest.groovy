85f306790784f19156cf6e6538b22bdb0e65c57b
apryamostanov <anton.pryamostanov@gmail.com>
2018-12-13 18:49:42 +0400

    Travis build should work now

--- src/test/groovy/io/infinite/bobbin/tests_1_x_x/BasicTest.groovy
+++ src/test/groovy/io/infinite/bobbin/tests_1_x_x/BasicTest.groovy
@@ -18,11 +18,11 @@ class BasicTest {
         bobbinNameAdapter.info("info abcd1234")
         bobbinNameAdapter.debug("debug " + uuid)
         bobbinNameAdapter.trace("trace " + uuid)
-        File file = TestTools.getResourceFile("BasicTest.log")
+        File file = TestTools.getResourceFile("BasicTest.expected")
         assert file != null
         Template template = TestTools.simpleTemplateEngine.createTemplate(file)
-        assert new File("./LOGS/BasicTest/ALL_LEVELS/BasicTest.log").exists()
-        assert new File("./LOGS/BasicTest/ALL_LEVELS/BasicTest.log").getText() == template.make(["uuid": uuid]).toString()
+        assert new File("./LOGS/BasicTest/ALL_LEVELS/BasicTest.expected").exists()
+        assert new File("./LOGS/BasicTest/ALL_LEVELS/BasicTest.expected").getText() == template.make(["uuid": uuid]).toString()
     }
 
     String getBobbinConfFileName() {
